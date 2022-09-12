80731566c7bd3cbbf481786242d45e312fb3da66
Mateus Coripio <69567720+mateuscoripiom@users.noreply.github.com>
2022-09-12 08:10:08 -0300

    Update dbEscola.sql

--- dbEscola.sql
+++ dbEscola.sql
@@ -52,3 +52,40 @@ insert into tb_Cliente(cliNome,cliEmail)
 	values("Tontinho", "tonti@escola.com");
     
 select * from tb_ClienteHistorico;
+
+Delimiter //
+create trigger TRG_CliHistoricoAtua after update on tb_Cliente
+	for each row 
+begin 
+	insert into tb_ClienteHistorico 
+		set clienteId = New.clienteId,
+		  cliNome = New.cliNome,
+         cliEmail = New.cliEmail,
+       Momento = current_timestamp(),
+          Situacao = "Depois";
+	insert into tb_ClienteHistorico 
+		set clienteId = Old.clienteId,
+		  cliNome = Old.cliNome,
+         cliEmail = Old.cliEmail,
+       Momento = current_timestamp(),
+          Situacao = "Antes";
+end // 
+    
+select * from tb_Cliente;
+select * from tb_ClienteHistorico;
+
+delimiter $$
+create procedure spAtualizarClienteNome(vcliNome varchar(150), vNomeAntigo varchar(150))
+begin
+	update tb_Cliente
+    set cliNome = vcliNome where vNomeAntigo = cliNome; 
+end $$
+
+drop procedure spAtualizarClienteNome;
+
+call spAtualizarClienteNome("Muito Tontinho", "Tontinho");
+call spAtualizarClienteNome("Lindinha de Morrer", "Lindinha");
+
+
+
+
